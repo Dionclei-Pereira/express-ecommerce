de3cd2052fc67766b526183033ba17a552af2e81
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-10-28 11:09:38 -0300

    GET and POST users

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
+
+// Using express default json configuration
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
-app.get('/', (req, res) => {
+let users = [{
+    name: 'John',
+    age: 12
+}];
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello world!');
-}) 
+});
+
+app.get('/users', (req: Request, res: Response) => {
+    res.send(users);
+});
+
+app.post('/users', (req: Request, res: Response) => {
+    const user = req.body;
+    users.push(user);
+    res.send('User created successfully');
+});
