8411d292a4f9508305b07e79e1006f5aa2499d10
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-10-28 11:18:10 -0300

    GET by id

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express';
+import { User } from './interfaces/user.interface';
 
 const app = express();
 
@@ -9,9 +10,11 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
-let users = [{
+let id = 1;
+let users: User[] = [{
     name: 'John',
-    age: 12
+    email: 'john@gmail.com',
+    id: 0
 }];
 
 app.get('/', (req: Request, res: Response) => {
@@ -22,8 +25,15 @@ app.get('/users', (req: Request, res: Response) => {
     res.send(users);
 });
 
+app.get('/users/:id', (req: Request, res: Response) => {
+    const userId = Number(req.params.id);
+    let user = users.find(user => user.id === userId);
+    res.send(user);
+})
+
 app.post('/users', (req: Request, res: Response) => {
-    const user = req.body;
+    const user: User = req.body;
+    user.id = id++;
     users.push(user);
     res.send('User created successfully');
 });
