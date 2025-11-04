2719ae243b121fb3ed0b1f870bf28916b0845402
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-11-04 19:47:55 -0300

    Error handler middleware

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,54 +1,74 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { User } from '../interfaces/user.interface';
 import { getFirestore } from 'firebase-admin/firestore';
 
 export class UsersController {
 
-    static async findAll(req: Request, res: Response) {
-        const snapshot = await getFirestore().collection('users').get();
-        const users = snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
+    static async findAll(req: Request, res: Response, next: NextFunction) {
+        try {
+            const snapshot = await getFirestore().collection('users').get();
+            const users = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                };
+            });
+            res.send(users);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async findById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = req.params.id;
+
+            const doc = await getFirestore().collection('users').doc(userId).get();
+            let user = {
+                id: userId,
                 ...doc.data()
             };
-        });
-        res.send(users);
+
+            res.send(user);
+        } catch (error) {
+            next(error);
+        }
     }
 
-    static async findById(req: Request, res: Response) {
-        const userId = req.params.id;
-
-        const doc = await getFirestore().collection('users').doc(userId).get();
-        let user = {
-            id: userId,
-            ...doc.data()
-        };
-
-        res.send(user);
+    static async save(req: Request, res: Response, next: NextFunction) {
+        try {
+            const user: User = req.body;
+            await getFirestore().collection('users').add(user);
+            res.send('User created successfully');
+        } catch (error) {
+            next(error);
+        }
     }
 
-    static async save(req: Request, res: Response) {
-        const user: User = req.body;
-        await getFirestore().collection('users').add(user);
-        res.send('User created successfully');
+    static async update(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = req.params.id;
+            let user: User = req.body;
+
+            await getFirestore().collection('users').doc(userId).set({
+                name: user.name,
+                email: user.email
+            });
+            res.send('User updated successfully');
+        } catch (error) {
+            next(error);
+        }
     }
 
-    static async update(req: Request, res: Response) {
-        const userId = req.params.id;
-        let user: User = req.body;
-    
-        await getFirestore().collection('users').doc(userId).set({
-            name: user.name,
-            email: user.email
-        });
-        res.send('User updated successfully');
-    }
+    static async delete(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = req.params.id;
 
-    static async delete(req: Request, res: Response) {
-    const userId = req.params.id;
-    
-    await getFirestore().collection('users').doc(userId).delete();
+            await getFirestore().collection('users').doc(userId).delete();
 
-    res.send('User deleted succesfully');
+            res.status(204).end();
+        } catch (error) {
+            next(error);
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import { routes } from './routes/router.index';
 import { initializeApp } from 'firebase-admin/app';
+import { errorHandler } from './middlewares/error.handler.middleware';
 
 const app = express();
 initializeApp();
 
 routes(app);
+errorHandler(app);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
